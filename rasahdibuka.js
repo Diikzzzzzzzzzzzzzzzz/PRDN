898d942adf675e2947ff9654fd3d0a2cec69d2ac
Mas Mbelo <117719643+thehashslinging@users.noreply.github.com>
2024-05-09 11:20:17 +0700

    Create rasahdibuka.js

---
+++ rasahdibuka.js
@@ -0,0 +1,19 @@
+const pesanLucu = [
+    "Dita itu seperti JavaScript, membuat hatiku selalu berdebar-debar.",
+    "Dita itu seperti kucing, selalu membuat saya terpesona dengan tatapan matanya.",
+    "Kalau Dita adalah angin, saya akan menjadi layang-layang yang tak bisa berhenti berputar di sekelilingnya.",
+    "Dita itu seperti kopi, membuat saya selalu ingin menikmati setiap tetes kebahagiaannya.",
+    "Dita itu seperti buku cerita, membawa saya ke petualangan yang tak pernah saya bayangkan sebelumnya.",
+    "Dita itu seperti musik, setiap nada dan melodi membuat hati saya terpukau.",
+    "Kalau Dita adalah algoritma, saya akan terus mencoba memecahkan teka-teki hatinya.",
+    "Dita itu seperti bintang di langit, selalu menerangi malam saya dengan kehangatan dan keindahannya.",
+    "Dita itu seperti permen, membuat hidup saya selalu manis meskipun pahitnya hari-hari.",
+    "BTW kui aku generate ning chatgpt akaowkoakwkaow.",
+];
+function pesanLucuDita() {
+    const randomIndex = Math.floor(Math.random() * pesanLucu.length);
+    return pesanLucu[randomIndex];
+}
+
+// Memunculkan pesan lucu tentang Dita
+console.log(pesanLucuDita());
